f2607ae394bf22a098dc72194c8c6847f28f944f
miquelven <miquelven.silva@gmail.com>
2024-07-08 12:41:04 -0300

    feat: create sectioncontenttext component

---
+++ src/components/SectionContentText/index.vue
@@ -0,0 +1,9 @@
+<script setup></script>
+
+<template>
+  <p
+    class="mx-auto max-w-3xl text-lg font-light leading-9 max-md:text-base max-md:leading-7 max-sm:text-sm max-sm:leading-7"
+  >
+    <slot />
+  </p>
+</template>
