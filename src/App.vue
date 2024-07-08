41c045d3affe4e63f9981c7c6a516bc1f76de955
miquelven <miquelven.silva@gmail.com>
2024-07-08 11:27:55 -0300

    feat: add about component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 import Header from "./components/ui/Header/index.vue";
 import Banner from "./components/Banner/index.vue";
+import About from "./components/About/index.vue";
 </script>
 
 <template>
   <Header />
   <Banner />
+  <About />
 </template>
