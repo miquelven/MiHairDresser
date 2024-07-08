91760f95c88efb6b1a656f90f725b99541cd4dfc
miquelven <miquelven.silva@gmail.com>
2024-07-08 12:42:10 -0300

    feat: create servicesdata

---
+++ src/data/servicesData.js
@@ -0,0 +1,40 @@
+const servicesData = [
+  {
+    id: 1,
+    icon: "bi-people-fill",
+    title: "Profissionais Qualificados",
+    text: "Nossa equipe possui anos de experiência e está sempre atualizada com as últimas tendências e técnicas.",
+  },
+  {
+    id: 2,
+    icon: "bi-telephone-fill",
+    title: "Atendimento Personalizado",
+    text: "Oferecemos um atendimento personalizado, ouvindo suas preferências para alcançar o resultado desejado.",
+  },
+  {
+    id: 3,
+    icon: "bi-emoji-smile-fill",
+    title: "Ambiente Acolhedor",
+    text: "Nosso salão é um refúgio de tranquilidade, projetado para proporcionar uma experiência relaxante",
+  },
+  {
+    id: 4,
+    icon: "bi-bag-check-fill",
+    title: "Produtos de Alta Qualidade",
+    text: "Utilizamos apenas produtos de alta qualidade, conhecidos por sua eficácia e segurança.",
+  },
+  {
+    id: 5,
+    icon: "bi-person-workspace",
+    title: "Variedade de Serviços",
+    text: "Desde cortes modernos até tratamentos capilares avançados, temos a solução ideal para você.",
+  },
+  {
+    id: 6,
+    icon: "bi-hand-thumbs-up-fill",
+    title: "Compromisso com a Satisfação",
+    text: "Sua satisfação é nossa prioridade. Esforçamo-nos para superar suas expectativas em cada visita.",
+  },
+];
+
+export default servicesData;
