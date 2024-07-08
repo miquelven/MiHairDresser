542aca4aa672dfb63ba0625fcad8cd10746c5468
miquelven <miquelven.silva@gmail.com>
2024-07-08 12:41:20 -0300

    feat: create sectionsubtitle component

---
+++ src/components/SectionSubTitle/index.vue
@@ -0,0 +1,9 @@
+<script setup></script>
+
+<template>
+  <h3
+    class="text-6xl font-bold w-3/4 max-2xl:w-full max-md:text-5xl max-sm:text-4xl max-sm:leading-10"
+  >
+    <slot />
+  </h3>
+</template>
