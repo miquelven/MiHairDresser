bbcd0c67d1e2cdd7d30c6dd5a6da8354ac62a8a7
miquelven <miquelven.silva@gmail.com>
2024-07-08 12:40:28 -0300

    feat: add add servicesarea component

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,12 @@
 import Header from "./components/ui/Header/index.vue";
 import Banner from "./components/Banner/index.vue";
 import About from "./components/About/index.vue";
+import ServicesArea from "./components/ServicesArea/index.vue";
 </script>
 
 <template>
   <Header />
   <Banner />
   <About />
+  <ServicesArea />
 </template>
