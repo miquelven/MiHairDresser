4afd38ffd95c5bf6cee9ec6b37218bbaad0e6113
miquelven <miquelven.silva@gmail.com>
2024-07-08 08:09:45 -0300

    chore: setup of project

---
+++ src/App.vue
@@ -0,0 +1,5 @@
+<script setup></script>
+
+<template>
+  <h1>Hello world</h1>
+</template>
---
+++ src/main.js
@@ -0,0 +1,13 @@
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+
+import { OhVueIcon, addIcons } from "oh-vue-icons";
+import { PxMenu } from "oh-vue-icons/icons";
+
+addIcons(PxMenu);
+
+const app = createApp(App);
+
+app.component("v-icon", OhVueIcon);
+app.mount("#app");
