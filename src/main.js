3ccb29f5eed41694afacbab539e81919200a217e
miquelven <miquelven.silva@gmail.com>
2024-07-08 12:40:36 -0300

    feat: add icons vue

--- src/main.js
+++ src/main.js
@@ -4,9 +4,29 @@ import "./main.css";
 import App from "./App.vue";
 
 import { OhVueIcon, addIcons } from "oh-vue-icons";
-import { PxMenu, BiX, BiScissors } from "oh-vue-icons/icons";
+import {
+  PxMenu,
+  BiX,
+  BiScissors,
+  BiPeopleFill,
+  BiTelephoneFill,
+  BiHandThumbsUpFill,
+  BiEmojiSmileFill,
+  BiBagCheckFill,
+  BiPersonWorkspace,
+} from "oh-vue-icons/icons";
 
-addIcons(PxMenu, BiX, BiScissors);
+addIcons(
+  PxMenu,
+  BiX,
+  BiScissors,
+  BiPeopleFill,
+  BiTelephoneFill,
+  BiHandThumbsUpFill,
+  BiEmojiSmileFill,
+  BiBagCheckFill,
+  BiPersonWorkspace
+);
 
 const app = createApp(App);
 
