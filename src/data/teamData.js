f62f1f4a5d023938b8fc4fe4b20f13b0c5a095c9
miquelven <miquelven.silva@gmail.com>
2024-07-09 09:21:41 -0300

    feat: create teamdata file

---
+++ src/data/teamData.js
@@ -0,0 +1,29 @@
+import womenHairdresser from "../assets/imgs/womenHairdresser.png";
+import hairdresser from "../assets/imgs/hairdresser.png";
+import barber from "../assets/imgs/barberTeam.png";
+
+const teamData = [
+  {
+    id: 1,
+    src: hairdresser,
+    alt: "Imagem do cabeleireiro",
+    title: "Lucas Ferreira",
+    function: "Cabeleireiro",
+  },
+  {
+    id: 2,
+    src: womenHairdresser,
+    alt: "Imagem do cabeleireira",
+    title: "Mariana Oliveira",
+    function: "Cabeleireira",
+  },
+  {
+    id: 3,
+    src: barber,
+    alt: "Imagem do barbeiro",
+    title: "Carlos Santos",
+    function: "Barbeiro",
+  },
+];
+
+export { teamData };
