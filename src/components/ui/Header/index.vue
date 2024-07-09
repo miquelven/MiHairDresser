dd4ef743e7820d6bcb735171fe7eec20f9bf2c86
miquelven <miquelven.silva@gmail.com>
2024-07-09 09:20:20 -0300

    feat: add image of the website logo

--- src/components/ui/Header/index.vue
+++ src/components/ui/Header/index.vue
@@ -2,6 +2,8 @@
 import Container from "../../Container/index.vue";
 import Menu from "./Menu/index.vue";
 
+import logoImg from "../../../assets/imgs/logo.png";
+
 import { ref, onMounted, onUnmounted } from "vue";
 
 const menuFixed = ref(false);
@@ -25,12 +27,25 @@ onUnmounted(() => {
 
 <template>
   <header
-    class="mt-10 text-[#c5c3c6] z-20"
-    :class="`${menuFixed ? 'fixed top-0 left-0 right-0' : 'relative '}`"
+    class="text-[#c5c3c6] z-20"
+    :class="`${
+      menuFixed
+        ? 'fixed top-0 left-0 right-0  bg-transparent backdrop-blur-lg  py-4'
+        : 'relative mt-10 '
+    }`"
   >
     <Container>
       <div class="flex justify-between items-center">
-        <h1>Logo</h1>
+        <a
+          href="#"
+          class="transition-all duration-300 brightness-75 hover:brightness-100"
+        >
+          <img
+            :src="logoImg"
+            alt="Imagem da logo do site"
+            class="w-20 h-20 object-cover invert max-sm:w-10 max-sm:h-10"
+          />
+        </a>
         <Menu />
       </div>
     </Container>
