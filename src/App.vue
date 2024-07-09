46d9518c107d854f423c9da7217eee5f18cbbb53
miquelven <miquelven.silva@gmail.com>
2024-07-09 09:16:55 -0300

    feat: add components

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,12 @@ import Header from "./components/ui/Header/index.vue";
 import Banner from "./components/Banner/index.vue";
 import About from "./components/About/index.vue";
 import advantageArea from "./components/advantageArea/index.vue";
+import ServicesArea from "./components/ServicesArea/index.vue";
+import TeamArea from "./components/TeamArea/index.vue";
+import PricingArea from "./components/PricingArea/index.vue";
+import ReviewsArea from "./components/ReviewsArea/index.vue";
+import ToTopButton from "./components/ToTopButton/index.vue";
+import Footer from "./components/ui/Footer/index.vue";
 </script>
 
 <template>
@@ -10,4 +16,10 @@ import advantageArea from "./components/advantageArea/index.vue";
   <Banner />
   <About />
   <advantageArea />
+  <ServicesArea />
+  <TeamArea />
+  <PricingArea />
+  <ReviewsArea />
+  <ToTopButton />
+  <Footer />
 </template>
