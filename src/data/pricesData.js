1cea2db4f5f4c7f28354f73867b5a3aabc4752cf
miquelven <miquelven.silva@gmail.com>
2024-07-09 09:20:58 -0300

    feat: create teamdata file

---
+++ src/data/pricesData.js
@@ -0,0 +1,32 @@
+const pricesData = [
+  {
+    id: 1,
+    title: "Básico",
+    benefits: [
+      "Corte de cabelo simples",
+      "Estilização",
+      "Correção de cor",
+      "Tranças",
+    ],
+    value: "R$30,00",
+  },
+  {
+    id: 2,
+    title: "Padrão",
+    benefits: [
+      "Estilização",
+      "Corte de cabelo simples",
+      "Correção de cor",
+      "Permanentes",
+    ],
+    value: "R$60,00",
+  },
+  {
+    id: 3,
+    title: "Premium",
+    benefits: ["Permanentes", "Extensões", "Estilização", "Tranças"],
+    value: "R$90,00",
+  },
+];
+
+export default pricesData;
