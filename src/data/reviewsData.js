1a4af3c7cd44d4a963744132b480150c464607ef
miquelven <miquelven.silva@gmail.com>
2024-07-09 09:21:10 -0300

    feat: create reviewsdata file

---
+++ src/data/reviewsData.js
@@ -0,0 +1,53 @@
+import reviewImg1 from "../assets/imgs/review1.png";
+import reviewImg2 from "../assets/imgs/review2.png";
+import reviewImg3 from "../assets/imgs/review3.png";
+import reviewImg4 from "../assets/imgs/review4.png";
+import reviewImg5 from "../assets/imgs/review5.png";
+import reviewImg6 from "../assets/imgs/review6.jpg";
+
+const reviewsData = [
+  {
+    id: 1,
+    title: "Thiago Souza",
+    text: "Ótimo atendimento e profissionais muito qualificados. Adorei o resultado!",
+    src: reviewImg1,
+    alt: "Imagem de uma pessoa sorrindo",
+  },
+  {
+    id: 2,
+    title: "Lucas Almeida",
+    text: "Serviço excelente e ambiente acolhedor. Recomendo a todos!",
+    src: reviewImg2,
+    alt: "Imagem de uma pessoa sorrindo",
+  },
+  {
+    id: 3,
+    title: "Bruno Carvalho",
+    text: "Profissionais competentes e um ambiente super agradável. Fiquei muito satisfeita!",
+    src: reviewImg3,
+    alt: "Imagem de uma pessoa sorrindo",
+  },
+  {
+    id: 4,
+    title: "Pedro Santos",
+    text: "Atendimento rápido e eficiente. Saí muito satisfeito com o corte.",
+    src: reviewImg4,
+    alt: "Imagem de uma pessoa sorrindo",
+  },
+  {
+    id: 5,
+    title: "Camila Ferreira",
+    text: "Equipe muito atenciosa e o salão é super bem decorado. Adorei o meu novo visual!",
+    src: reviewImg5,
+    alt: "Imagem de uma pessoa sorrindo",
+  },
+  {
+    id: 6,
+    title: "Rafael Lim",
+    text: "Corte impecável e preço justo. Com certeza voltarei mais vezes!",
+    src: reviewImg6,
+    alt: "Imagem de uma pessoa sorrindo",
+  },
+];
+
+export default reviewsData;
