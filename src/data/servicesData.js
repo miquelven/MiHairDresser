84a65c10cba432f12d31393a198f1ad202d25c88
miquelven <miquelven.silva@gmail.com>
2024-07-09 09:21:19 -0300

    feat: create servicesdata file

---
+++ src/data/servicesData.js
@@ -0,0 +1,29 @@
+import manCutImg from "../assets/imgs/manCut.png";
+import womenCutImg from "../assets/imgs/womenCut.png";
+import barberImg from "../assets/imgs/barber.png";
+
+const servicesData = [
+  {
+    id: 1,
+    src: manCutImg,
+    alt: "Imagem de um cabeleireiro cortando o cabelo de um homem",
+    title: "Corte Masculino",
+    text: " Transforme seu visual com cortes masculinos feitos sob medida para realçar sua personalidade e estilo único.",
+  },
+  {
+    id: 2,
+    src: womenCutImg,
+    alt: "Imagem de um cabeleireiro cortando o cabelo de uma mulher",
+    title: "Corte Feminino",
+    text: "Descubra cortes femininos que combinam elegância e modernidade, criados para refletir sua beleza natural e individualidade.",
+  },
+  {
+    id: 3,
+    src: barberImg,
+    alt: "Imagem de um cabeleireiro cortando a barba de um homem",
+    title: "Corte de barba",
+    text: "Deixe nossa equipe especializada cuidar da sua barba com cortes que garantem um visual impecável e bem cuidado, respeitando sua preferência e estilo pessoal.",
+  },
+];
+
+export default servicesData;
