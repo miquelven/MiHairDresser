c8d7c7757fb0255b4a03377f0028b15c8cb8fc75
miquelven <miquelven.silva@gmail.com>
2024-07-09 09:18:22 -0300

    feat: create carouselcontainer component

---
+++ src/components/CarouselContainer/index.vue
@@ -0,0 +1,15 @@
+<script setup>
+import "vue3-carousel/dist/carousel.css";
+import { Carousel, Pagination, Navigation } from "vue3-carousel";
+</script>
+
+<template>
+  <Carousel :transition="1500" :itemsToShow="1" :navigationEnabled="true">
+    <slot />
+
+    <template #addons>
+      <Pagination />
+      <Navigation />
+    </template>
+  </Carousel>
+</template>
